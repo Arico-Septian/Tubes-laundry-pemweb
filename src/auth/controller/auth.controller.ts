2e8d6ee51236c2259cdda2f40fed24cf0f75a6ec
Arico Septian <aricoseptian5@gmail.com>
2023-12-14 20:50:48 +0700

    menambah router

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -4,10 +4,31 @@ import { RegistrasiDto } from '../dto/registrasi.dto';
 import { LoginAuhDTO } from '../dto/login-auth.dto';
 import { User } from 'src/users/entities/user.entity';
 import { UpdateUserDto } from 'src/users/dto/update-user.dto';
+import { Role } from 'src/enum/role.enum';
+import { Roles } from 'src/decorators/role.decorator';
+import Serialize from 'src/interceptors/serialize.interceptor';
+import { UserDTO } from 'src/users/dto/user.dto';
+import { CreateUserDTO } from 'src/users/dto/create-user.dto';
+
 
 @Controller('api')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Roles(Role.Admin)
+  @Serialize(UserDTO)
+  @Post('auth/register/admin')
+  async registerAdmin(@Body() body: CreateUserDTO): Promise<User> {
+    return await this.authService.createAdmin(body)
+  }
+
+  // route for make user with role moderator
+  @Serialize(UserDTO)
+  @Roles(Role.Admin)
+  @Post('auth/register/moderator')
+  async registerModerator(@Body() body: CreateUserDTO): Promise<User> {
+    return await this.authService.createModerator(body)
+  }
 
   // registrasi user baru
   @Post('auth/register')
@@ -28,7 +49,7 @@ export class AuthController {
     @Body() body: UpdateUserDto): Promise<User> {
     return this.authService.update(userid, body);
   }
-  
+
   // melihat data user berdasarkan userid
   @Get('user/:userid')
   findOne(@Param('userid') userid: string): Promise<User[] | null> {
