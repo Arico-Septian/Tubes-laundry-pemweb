9f6119b34b51d2aa61726a97cb2b8a261575d3b1
Arico Septian <aricoseptian5@gmail.com>
2023-12-31 15:02:21 +0700

    token

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:4000', // specify the allowed origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // allow sending cookies and credentials
+  });
   await app.listen(3000);
 }
 bootstrap();
